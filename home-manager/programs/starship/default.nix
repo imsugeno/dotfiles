37d36d8d749eebf4e754fc986a013de89546a488
imsugeno <g.tokyo.kazusa@gmail.com>
2026-02-14 15:44:51 +0900

    add: starship setting

--- home-manager/programs/starship/default.nix
+++ home-manager/programs/starship/default.nix
@@ -1,8 +1,15 @@
 { config, pkgs, lib, ... }:
 
+let
+  dotfilesPath = "${config.home.homeDirectory}/repos/github.com/imsugeno/dotfiles";
+in
 {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+  };
+
+  xdg.configFile."starship.toml" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/home-manager/programs/starship/starship.toml";
   };
 }
